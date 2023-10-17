bab91bacb0f858233c97240e2c32d49a2a65fe82
Raghad-alshaita <raghadalsh99@gmail.com>
2023-10-17 12:08:07 +0200

    -- add-animation

--- src/app/pipes/summary.pipe.ts
+++ src/app/pipes/summary.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SummaryPipe implements PipeTransform {
 
   transform(value: string, limit?: number): any {
-    // if (!value) return null;
-    // let acctuallimit = (limit) ? limit : 20;
-    // if (value.length > acctuallimit)
-      return value.substring(0, 40) + '..'
-    // else return value
+    if (!value) return null;
+    let acctuallimit = (limit) ? limit : 20;
+    if (value.length > acctuallimit)
+      return value.substring(0, acctuallimit) + '..'
+    else return value
 
 
   }
